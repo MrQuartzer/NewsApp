aaab80c8a9a4eb857c76c0838b30c2f19792dced
Mrquartzer <nantawat_yai@hotmail.com>
2023-11-09 14:32:13 +0700

    add BlogTile to home page

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/Models/article_model.dart';
-import 'package:newsapp/Helps//news.dart';
+import 'package:newsapp/Helps/news.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -50,17 +50,80 @@ class _HomeState extends State<Home> {
         elevation: 0.0,
       ),
       body: _loading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(articles[index].title),
-            subtitle: Text(articles[index].description),
-            // Add more details or functionalities here
-            // e.g., onTap to navigate to a detailed view of the article
-          );
-        },
+          ? Center(
+          child: Container(
+            child: CircularProgressIndicator(),
+          ))
+          : SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 16),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        itemCount: articles.length,
+                        itemBuilder: (context, index) {
+                          return BlogTile(
+                          imageUrl: articles[index].urlToImage,
+                          title: articles[index].title,
+                          description: articles[index].description,
+                          url: articles[index].url,
+                        );
+                      }),
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, description, url;
+  BlogTile(
+      {required this.imageUrl, required this.title, required this.description, required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Navigator.push(context, MaterialPageRoute(
+        //     builder: (context) => Article_View(
+        //       blogUrl: url,
+        //     )
+        // ));
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 16),
+        child: Column(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(imageUrl),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(
+              description,
+              style: TextStyle(color: Colors.black26),
+            ),
+          ],
+        ),
       ),
     );
   }
